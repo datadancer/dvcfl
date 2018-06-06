19d34b65340997c089c3528dd715e80b049d7138
datadancer <datadancer@163.com>
2018-06-06 10:00:34 +0800

    Read kallsyms into a list and output all the ioctl function of all devices in device_address

--- getdvcfl.c
+++ getdvcfl.c
@@ -16,6 +16,14 @@ struct fname_ptr{
 	char fname[512];
 	unsigned long ptr;
 };
+struct ksym{
+    unsigned long addr;
+    char tag;
+    char *name;
+    char *module;
+    struct ksym * next;
+};
+
 static struct fname_ptr devs[1000];
 static int dev_cnt = 0;
 
@@ -42,8 +50,6 @@ int handle_dir(int fd, char *s_dir){
 	    strcat(fname, s_dir); strcat(fname, "/");
 	    strcat(fname, dir->d_name);
         printf("Try to open %s\n", fname);
-        //if(strncmp(fname, "/dev/snd/", 8) == 0) continue;
-        //if(strncmp(fname, "/dev/tty", 8) == 0) continue;
     	fdp.fd = open(fname, O_RDONLY);
     	if(fdp.fd < 0) { printf("Open %s error.\n", fname); continue;}
     	if (ioctl(fd, 0x40086602, &fdp) == -1) {perror("ioctl");}
@@ -62,9 +68,66 @@ int handle_dir(int fd, char *s_dir){
     }
 }
 
+struct ksym * read_kallsyms(){
+    struct ksym *head;
+    struct ksym *cur;
+    ssize_t read_size;
+    size_t len;
+    char * line = NULL;
+    char * tmp= NULL;
+    FILE * kallsyms = fopen("kallsyms", "r");
+
+    head = (struct ksym *)malloc(sizeof(struct ksym));
+    cur = head;
+
+    while((read_size = getline(&line, &len, kallsyms)) != -1 ){
+        tmp = strtok(line, " \t\n");
+        cur->addr = strtoull(tmp, NULL, 16);
+        cur->tag = strtok(NULL, " \t\n")[0];
+
+        tmp = strtok(NULL, " \t\n");
+        if(tmp != NULL){
+            cur->name = (char *)malloc(strlen(tmp)+1);
+            strcpy(cur->name, tmp);
+        } else {
+            cur->name = NULL;
+        }
+
+        tmp = strtok(NULL, " \t\n");
+        if(tmp != NULL){
+            cur->module = (char *)malloc(strlen(tmp)+1);
+            strcpy(cur->module, tmp);
+        } else {
+            cur->module = NULL;
+        }
+
+        cur->next = (struct ksym *)malloc(sizeof(struct ksym));
+        cur = cur->next;
+    }
+
+    cur->next = NULL;
+    cur = head;
+ #ifdef DEBUG
+    while(cur != NULL){
+        printf("%lx %c %s %s\n", cur->addr, cur->tag, cur->name, cur->module);
+        cur = cur->next;
+    }
+#endif
+    fclose(kallsyms);
+    return head;
+}
+
+struct ksym * find_ksym_by_addr(struct ksym *head, unsigned long addr){
+    while(head != NULL) if(head->addr == addr) return head; else head=head->next;
+    return NULL;
+}
+
 int main(int argc, char** argv)  
 {  
     int fd = -1;
+    struct ksym *head = read_kallsyms();
+    //struct ksym *head = NULL;
+    struct ksym *cur= head;
     
     fd = open(DVCFL_DEVICE, O_RDWR);  
     if(fd == -1) {  
@@ -73,30 +136,36 @@ int main(int argc, char** argv)
     }  
     handle_dir(fd, "/dev");
     unsigned long addr; char tc; char func_name[256]; int i;
-    FILE * kallsyms = fopen("kallsyms", "r");
     FILE * syms_devs = fopen("syms_devs", "w");
     FILE * device_address = fopen("device_address", "w");
 
 	for(i=0;i<dev_cnt;i++) {
-        fprintf(device_address, "%s %lx\n", devs[i].fname, devs[i].ptr); 
+        cur = find_ksym_by_addr(head, devs[i].ptr);
+        if(cur != NULL){
+            if(cur->module == NULL)
+            fprintf(device_address, "%s %s\n", devs[i].fname, cur->name); 
+            else
+            fprintf(device_address, "%s %s %s\n", devs[i].fname, cur->name, cur->module); 
+        }
     }
 
-    if(kallsyms == NULL || syms_devs==NULL ) perror("Open kallsyms Error");
+    if(syms_devs==NULL ) perror("Open kallsyms Error");
     else{
-	while(fscanf(kallsyms, "%lx %c %s\n", &addr, &tc, func_name) == 3){
-	    //if(tc == 'T' || tc == 't') printf("%lx %c %s\n", addr, tc, func_name);
+    while(cur!=NULL){
 	    for(i=0;i<dev_cnt;i++) {
-                if(addr == devs[i].ptr) { 
-                        fprintf(stdout, "%s %s\n", devs[i].fname, func_name); 
-                        fprintf(syms_devs, "%s %s\n", devs[i].fname, func_name); break;
-                }
+            if(cur->addr == devs[i].ptr) { 
+                fprintf(stdout, "%s %s\n", devs[i].fname, cur->name); 
+                fprintf(syms_devs, "%s %s\n", devs[i].fname, cur->name); 
+                break;
+            }
         }
+        cur=cur->next;
 	}
     }
-    fclose(kallsyms);
+    cur = head;
+    while(cur!=NULL){free(cur->name); free(cur->module); cur=cur->next;}
     fclose(syms_devs);
     fclose(device_address);
-    
     close(fd);  
     return 0;  
 }  
